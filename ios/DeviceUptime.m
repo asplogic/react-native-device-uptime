c32a098b2c396e232bfaaad8fc1d25058dffda40
asplogic <hon@pintracer.com>
2024-03-11 09:29:33 -0700

    Fix ios uptime

--- ios/DeviceUptime.m
+++ ios/DeviceUptime.m
@@ -1,14 +1,38 @@
 #import "DeviceUptime.h"
 #import <React/RCTLog.h>
 
+#include <sys/sysctl.h>
+
 @implementation DeviceUptime
 
 RCT_EXPORT_MODULE()
 
 RCT_EXPORT_METHOD(getUptime:(RCTPromiseResolveBlock)resolve rejecter:(RCTPromiseRejectBlock)reject)
 {
-  NSTimeInterval uptime = [[NSProcessInfo processInfo] systemUptime];
-  resolve([NSString stringWithFormat:@"%f", uptime]);  
+  resolve([NSString stringWithFormat:@"%ld", [self uptime]]); 
+}
+
+/**
+ * Get the system up time in seconds
+ *
+ * Method originally from here:
+ * http://stackoverflow.com/questions/12488481/getting-ios-system-uptime-that-doesnt-pause-when-asleep
+ */
+- (time_t)uptime
+{
+    struct timeval boottime;
+    int mib[2] = {CTL_KERN, KERN_BOOTTIME};
+    size_t size = sizeof(boottime);
+    time_t now;
+    time_t uptime = -1;
+
+    (void) time(&now);
+
+    if (sysctl(mib, 2, &boottime, &size, NULL, 0) != -1 && boottime.tv_sec != 0) {
+        uptime = now - (boottime.tv_sec);
+    }
+
+    return uptime;
 }
 
 @end
